36e172ff68b5cca29d2d146a285f2905972ada1d
Dylan Leard <dleard@uvic.ca>
2019-02-14 19:36:51 +0000

    refactor Message.jsx

--- src/Message.jsx
+++ src/Message.jsx
@@ -2,36 +2,17 @@ import React, {Component} from 'react';
 
 class Message extends Component {
 
-  renderImages = () => {
-    const imageRegex = /[^\ ]*\.(?:jpg|gif|png)/
-    const {message} = this.props
-    if (message.content) {
-      const foundImages = message.content.match(imageRegex);
-      if (foundImages !== null) {
-        const Images = foundImages.map((image) => {
-          return ( 
-            <div style = {{color: `${message.color}`}} className="message">
-              <span className="message-username">{message.username}</span>
-              <img className='embeddedImage' src='https://upload.wikimedia.org/wikipedia/en/thumb/d/d0/JaketheDog.png/220px-JaketheDog.png' />
-            </div>
-          )
-        });
-        return Images;
-      }
-    }
-    
-  } 
+  handleMessage = (message) => {
+    return (
+      <div style = {{color: `${message.color}`}} className="message">
+        <span className="message-username">{message.username}</span>
+        <span className="message-content">{message.content}</span>
+      </div>
+    )
+  }
 
-  renderMessage = () => {
-    const {message} = this.props;
-    if (message.type === 'incomingMessage') {
-      return (
-        <div style = {{color: `${message.color}`}} className="message">
-          <span className="message-username">{message.username}</span>
-          <span className="message-content">{message.content}</span>
-        </div>
-      )
-    } else if (message.type === 'incomingNotification' && message.currentUser === 'AnonymousAndy') {
+  handleNotification = (message) => {
+    if (message.type === 'incomingNotification' && message.currentUser === 'AnonymousAndy') {
       return (
         <div className="notification">
           <span className="notification-content"><strong>{message.username}</strong> has joined the chat!</span>
@@ -43,6 +24,35 @@ class Message extends Component {
           <span className="notification-content"><strong>{message.currentUser}</strong> has changed their name to <strong>{message.username}</strong></span>
         </div> 
       )
+    }
+  }
+  
+  renderImages = () => {
+    const imageRegex = /[^\ ]*\.(?:jpg|gif|png)/
+    const {message} = this.props
+    if (message.content) {
+      const foundImages = message.content.match(imageRegex);
+      if (foundImages !== null) {
+        const Images = foundImages.map((image) => {
+          return ( 
+            <div style = {{color: `${message.color}`}} className="message">
+              <span className="message-username">{message.username}</span>
+              <img className='embeddedImage' src={image} />
+            </div>
+          )
+        });
+        return Images;
+      }
+    }
+  } 
+
+  renderMessage = () => {
+    const {message} = this.props;
+    console.log(message);
+    if (message.type === 'incomingMessage') {
+      return this.handleMessage(message);
+    } else { 
+      return this.handleNotification(message);
     }
   }
   
